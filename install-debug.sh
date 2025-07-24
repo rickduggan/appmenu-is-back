c640944d6d7ebb3ed1dcbd8e8aa67304c3f60f56
Rick Duggan <rick@conciergesoftwaredesign.com>
2025-07-24 16:23:25 -0700

    minor mods to install script

--- install-debug.sh
+++ install-debug.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#cd ..
-#pwd
+cd ..
+pwd
 rm appmenu-is-back@rickduggan.shell-extension.zip
-gnome-extensions pack appmenu-is-back
+gnome-extensions pack --force appmenu-is-back@rickduggan 
 gnome-extensions uninstall appmenu-is-back@rickduggan
-gnome-extensions install appmenu-is-back@rickduggan.shell-extension.zip
+gnome-extensions install --force appmenu-is-back@rickduggan.shell-extension.zip
 echo 'Alt+F2 r'
 #read -p 'Alt+F2 r'
 #gnome-extensions enable appmenu-is-back@rickduggan
+cd appmenu-is-back@rickduggan
